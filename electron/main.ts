49de89b1458ca1c8cdd226c57ec7df945b16fbf9
bravefe_laptop <paipatcharak@gmail.com>
2026-09-11 00:23:09 +0700

    change ico location

--- electron/main.ts
+++ electron/main.ts
@@ -11,7 +11,7 @@ async function createWindow(): Promise<void> {
     height: 760,
     minWidth: 360,
     minHeight: 560,
-    icon: join(app.getAppPath(), "logo.png"),
+    icon: join(app.getAppPath(), "icon.ico"),
     title: "LEB2 Work Checker",
     webPreferences: {
       contextIsolation: true,
